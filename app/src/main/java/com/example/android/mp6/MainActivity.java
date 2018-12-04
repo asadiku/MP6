e21d21854ac0964e673860e91cc66c1427dff06b
asadiku <asadik3@illinois.edu>
2018-12-03 23:19:25 -0600

    Added Log message for button and a textbox for console selection.

--- app/src/main/java/com/example/android/mp6/MainActivity.java
+++ app/src/main/java/com/example/android/mp6/MainActivity.java
@@ -2,12 +2,24 @@ package com.example.android.mp6;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    /** Default logging tag for messages from the main activity. */
+    private static final String TAG = "MP6";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final Button compare = findViewById(R.id.button_compare);
+        compare.setOnClickListener(v -> {
+            Log.d(TAG, "Compare button clicked");
+        });
     }
 }
