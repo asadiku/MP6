f79fca11bc5bb95958e8c1a7eeca3cc4154dce30
tdbeirne <tdbeirne@yahoo.com>
2018-12-05 02:25:28 -0600

    Added second activity screen and added navigation capability from first to second screen.

--- app/src/main/java/com/example/android/mp6/MainActivity.java
+++ app/src/main/java/com/example/android/mp6/MainActivity.java
@@ -1,14 +1,17 @@
 package com.example.android.mp6;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
-
+    public static final String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
     /** Default logging tag for messages from the main activity. */
     private static final String TAG = "MP6";
 
@@ -18,8 +21,17 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         final Button compare = findViewById(R.id.button_compare);
-        compare.setOnClickListener(v -> {
-            Log.d(TAG, "Compare button clicked");
+        compare.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(v.getContext(), DisplayComparisonActivity.class);
+                EditText editText2 = (EditText) findViewById(R.id.editText2);
+                EditText editText3 = (EditText) findViewById(R.id.editText3);
+                String message2 = editText2.getText().toString();
+                String message3 = editText3.getText().toString();
+                intent.putExtra(EXTRA_MESSAGE, message2);
+                intent.putExtra(EXTRA_MESSAGE, message3);
+                startActivity(intent);
+            }
         });
     }
 }
