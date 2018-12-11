a3e9ec229efd2a0ba55879c346ab6ea6b6b9ccd8
asadiku <asadik3@illinois.edu>
2018-12-11 16:28:31 -0600

    Added another API call to receive second profile information.

--- app/src/main/java/com/example/android/mp6/MainActivity.java
+++ app/src/main/java/com/example/android/mp6/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.android.mp6;
 
+
+import android.os.Handler;
 import android.content.Intent;
+import android.os.Looper;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -18,6 +21,7 @@ import com.android.volley.toolbox.Volley;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.TimerTask;
 
 public class MainActivity extends AppCompatActivity {
     /** Default logging tag for messages from the main activity. */
@@ -61,6 +65,7 @@ public class MainActivity extends AppCompatActivity {
 
                 Log.d(TAG, "Compare button clicked");
                 startAPICall();
+                startAPICall2();
                 startActivity(intent);
             }
         });
@@ -99,5 +104,41 @@ public class MainActivity extends AppCompatActivity {
             e.printStackTrace();
         }
 
+    }
+    void startAPICall2() {
+        final Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(
+                            Request.Method.GET,
+                            "https://api.fortnitetracker.com/v1/profile/" + console + "/" + second_name,
+                            null,
+                            new Response.Listener<JSONObject>() {
+                                @Override
+                                public void onResponse(final JSONObject response) {
+                                    Log.d(TAG, response.toString());
+                                }
+                            }, new Response.ErrorListener() {
+                        @Override
+                        public void onErrorResponse(final VolleyError error) {
+                            Log.w(TAG, "There was an error in your request");
+                        }
+                    }) {
+                        @Override
+                        public Map<String, String> getHeaders() {
+                            Map<String, String> params = new HashMap<String, String>();
+                            params.put("TRN-Api-Key", "e7aae4fa-c500-4948-9fec-2d8f179f6b74");
+                            Log.d(TAG, params.toString());
+                            return params;
+                        }
+                    };
+                    requestQueue.add(jsonObjectRequest);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }, 2000);
     }
 }
